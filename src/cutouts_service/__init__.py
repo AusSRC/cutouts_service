b41dbba2ce58e428424f0b9a0713e440f3465d0b
Alex Williamson <alexander.williamson@csiro.au>
2026-06-22 18:01:08 +0800

    CUT-28: Added objstore as a directory for now as the original repo has not setup.py or equivalent.

--- src/cutouts_service/__init__.py
+++ src/cutouts_service/__init__.py
@@ -1,1 +1,5 @@
 """Basic package for cutouts-service."""
+
+from cutouts_service.objstore import FITSheader, get_access_keys, ObjStore, S3Object, URLObject
+
+__all__ = ["FITSheader", "get_access_keys", "ObjStore", "S3Object", "URLObject"]

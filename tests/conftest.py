0f8398814a03f35fab17d1655ae3dc02583d4b58
Alex Williamson <alexander.williamson@csiro.au>
2026-07-14 14:03:14 +0800

    added tests to check multitable functionality

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,6 +47,25 @@ def source_header_3d() -> fits.Header:
     header["BITPIX"] = -32
     return header
 
+@pytest.fixture
+def beam_table():
+    table_data = np.array([[1.7200001e+01, 1.4300000e+01, 8.1239998e+01, 0, 0],
+                  [1.7400000e+01, 1.4700000e+01, 7.6949997e+01, 1, 0],
+                  [1.7500000e+01, 1.4600000e+01, 8.4230003e+01, 2, 0],
+                  [1.7299999e+01, 1.4600000e+01, 8.1400002e+01, 3, 0],
+                  [1.7700001e+01, 1.4500000e+01, 7.7349998e+01, 4, 0],
+                  [1.7500000e+01, 1.4700000e+01, 7.9250000e+01, 5, 0],
+                  [1.7400000e+01, 1.4900000e+01, 7.8629997e+01, 6, 0],
+                  [1.7600000e+01, 1.4800000e+01, 7.9059998e+01, 7, 0],
+                  [1.7400000e+01, 1.5000000e+01, 8.0629997e+01, 8, 0],
+                  [1.6700001e+01, 1.4000000e+01, 8.6949997e+01, 9, 0]])
+    table_names = ('BMAJ', 'BMIN', 'BPA', 'CHAN', 'POL')
+    cols = []
+    for i, name in enumerate(table_names):
+        col = fits.Column(name=name, array=table_data[:, i], format='K')
+        cols.append(col)
+    return fits.BinTableHDU.from_columns(cols)
+
 
 @pytest.fixture
 def http_file_server(tmp_path: Path):
@@ -99,7 +118,7 @@ def remote_fits_3d(http_file_server, source_header_3d: fits.Header):
 def remote_fits_3d_objstore(http_file_server, source_header_3d: fits.Header):
     root = http_file_server["root"]
     base_url = http_file_server["base_url"]
-    source_file = root / "source_cube.fits"
+    source_file = root / "source_cube_objstore.fits"
     source_data = np.arange(10 * 1 * 20 * 20, dtype=np.float32).reshape((10, 1, 20, 20))
     source_header_3d_objstore = source_header_3d
     source_header_3d_objstore.set("NAXIS4", 1)
@@ -107,4 +126,19 @@ def remote_fits_3d_objstore(http_file_server, source_header_3d: fits.Header):
     return {
         "url": f"{base_url}/{source_file.name}",
         "header": source_header_3d_objstore,
+    }
+
+@pytest.fixture
+def remote_fits_3d_multitable(http_file_server, source_header_3d: fits.Header, beam_table):
+    root = http_file_server["root"]
+    base_url = http_file_server["base_url"]
+    source_header_3d.set("CASAMBM", True)
+    source_file = root / "source_cube_multitable.fits"
+    source_data = np.arange(10 * 2 * 20 * 20, dtype=np.float32).reshape((10, 2, 20, 20))
+    hdulist = [fits.PrimaryHDU(data=source_data, header=source_header_3d), beam_table]
+    hdulist = fits.HDUList(hdulist)
+    hdulist.writeto(source_file)
+    return {
+        "url": f"{base_url}/{source_file.name}",
+        "header": source_header_3d,
     }
